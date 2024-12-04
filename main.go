8ef914273f24708e788747860bf62fc7d30c5877
Philip Szalla <philip@szalla.de>
2024-12-04 01:00:18 +0000

    feat: add day02

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/philipszalla/adventofcode-2024/day01"
+import (
+	"github.com/philipszalla/adventofcode-2024/day01"
+	"github.com/philipszalla/adventofcode-2024/day02"
+)
 
 func main() {
 	day01.Part1("day01/puzzle.txt")
 	day01.Part2("day01/puzzle.txt")
+
+	day02.Part1("day02/puzzle.txt")
+	day02.Part2("day02/puzzle.txt")
 }
